d3d085acdf6a85743d8bd7bcc5997a60fee736f0
maksim-protsenko <protsenko.maksim.88@gmail.com>
2021-01-26 18:15:19 +0200

    structure of program was added

---
+++ src/main/java/com/company/Cursor16/Cursor16Application.java
@@ -0,0 +1,13 @@
+package com.company.Cursor16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursor16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursor16Application.class, args);
+	}
+
+}
