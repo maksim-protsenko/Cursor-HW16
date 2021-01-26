db4d1ff88f6c978c31f25d3c4117f57cb4d62aac
maksim-protsenko <protsenko.maksim.88@gmail.com>
2021-01-26 18:19:10 +0200

    autoconfigurations on class and on property were added

---
+++ src/main/java/com/company/Cursor16/configurations/ConfigurationEnablerClass.java
@@ -0,0 +1,11 @@
+package com.company.Cursor16.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationEnablerClass {
+
+    public ConfigurationEnablerClass() {
+        System.out.println("ConfigurationEnablerClass is existed, really!");
+    }
+}
